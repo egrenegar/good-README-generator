aed0891d56ac2a30155ad4fd38d2a2b662d4839f
Emily Renegar <egrenegar@gmail.com>
2020-05-22 21:37:17 -0400

    added rough template of generated README from user answers

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,36 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+${data.description}
+
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+![](https://img.shields.io/badge/LICENSE-MIT-<ORANGE>)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+* Profile picture from GitHub
+* ${data.email}
 `;
 }
 
